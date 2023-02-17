6a1f510698bd7f2dff4084aa45b2578c91056358
Cloggings-James <James.wanjirukenyan254@gmail.com>
2023-02-17 15:27:28 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int i;
+
+        i = 48;
+        while (i <= 57)
+        {
+                putchar(i);
+                i++;
+        }
+        putchar('\n');
+
+        return (0);
+}
+
+
