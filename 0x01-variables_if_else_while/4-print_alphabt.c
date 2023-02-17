80fdecba23bdf1038b7741a09e0aa461261516fe
Cloggings-James <James.wanjirukenyan254@gmail.com>
2023-02-17 07:03:20 -0800

    james

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase,
+ *              followed by a new line, except for 'q' and 'e'.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char letter = 'a';
+
+    while (letter <= 'z')
+    {
+        if (letter != 'q' && letter != 'e')
+            putchar(letter);
+        letter++;
+    }
+
+    putchar('\n');
+    return (0);
+}
+
