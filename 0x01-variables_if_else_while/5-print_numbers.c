86c07db5f490d8679f34cda1138259821494769a
Cloggings-James <James.wanjirukenyan254@gmail.com>
2023-02-17 15:22:17 -0800

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,7 +3,8 @@
 /**
  * main - Entry point
  *
- * Description: Prints all single digit numbers of base 10 starting from 0.
+ * Description: Prints all single digit numbers of base 10,
+ *              starting from 0, followed by a new line
  *
  * Return: Always 0 (Success)
  */
@@ -13,8 +14,8 @@ int main(void)
 
     for (i = 0; i < 10; i++)
         putchar(i + '0');
-
     putchar('\n');
+
     return (0);
 }
 
