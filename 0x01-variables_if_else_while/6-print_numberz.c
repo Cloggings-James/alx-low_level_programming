8247f2bc8ed49346f2e4f27dd89e3dd45a1d905e
Cloggings-James <James.wanjirukenyan254@gmail.com>
2023-02-18 04:13:14 -0800

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - prints all single digit numbers of base 10 starting from 0
+ * main - Entry point
  *
- * Return: 0 on success
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int i;
+        int i;
 
-    for (i = 0; i < 10; i++)
-    {
-        putchar(i + '0');
-    }
-    putchar('\n');
+        for (i = 0; i < 10; i++)
+        {
+                putchar(i + '0');
+        }
 
-    return (0);
+        putchar('\n');
+
+        return (0);
 }
 
