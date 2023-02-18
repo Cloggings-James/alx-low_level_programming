c3fbd1719f000955a3acd711f3bdb4eaafa09f51
Cloggings-James <James.wanjirukenyan254@gmail.com>
2023-02-18 03:23:51 -0800

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,24 +1,2 @@
-#include <unistd.h>
-
-/**
- * main - Entry point
- *
- * Description: Prints all single digit numbers of base 10 starting from 0
- * using only the putchar function, without any variables of type char
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-    int num = 48;
-
-    while (num <= 57)
-    {
-        putchar(num);
-        num++;
-    }
-    putchar('\n');
-
-    return (0);
-}
-
+#!/bin/bash
+BEST=School
