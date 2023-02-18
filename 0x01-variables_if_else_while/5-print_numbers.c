2fadf5ce2da92649c08dfac0eaabdcbb8c787557
Cloggings-James <James.wanjirukenyan254@gmail.com>
2023-02-18 03:51:02 -0800

    james

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - Prints all single digit numbers of base 10 starting from 0
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-    int i;
-
-    for (i = 0; i < 10; i++)
-    {
-        putchar(i + '0');
-    }
-    putchar('\n');
-
-    return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
-
