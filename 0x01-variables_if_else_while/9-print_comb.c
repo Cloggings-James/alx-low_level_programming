ebc45a252673fdebd50b43550ac069669c44410c
Cloggings-James <James.wanjirukenyan254@gmail.com>
2023-02-18 04:59:56 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible combinations of single-digit numbers
+ * separated by a comma and space
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
     int num;
