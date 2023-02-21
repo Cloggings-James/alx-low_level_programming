8d11a7b50383d4fff9e1e6018c7c688845b7e92a
Cloggings-James <James.wanjirukenyan254@gmail.com>
2023-02-21 13:50:30 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islowe.c
@@ -0,0 +1,14 @@
+#include <ctype.h>
+
+/**
+ * islower - checks if a character is lowercase
+ * @c: the character to be checked
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int islower(int c)
+{
+    /* Use the islower() function from ctype.h */
+    return islower(c) != 0;
+}
+
