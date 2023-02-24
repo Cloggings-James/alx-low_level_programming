60d63942940e07ed055bae54289c6df3d87c701b
Cloggings-James <James.wanjirukenyan254@gmail.com>
2023-02-24 02:23:14 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Calls the print_numbers function.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_numbers();
+    return (0);
+}
+
