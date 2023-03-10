96c6891c399826bb69c5e1b5c229ea582e976719
Cloggings-James <James.wanjirukenyan254@gmail.com>
2023-03-10 06:06:37 -0800

    james

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,146 +1,24 @@
 #include <stdio.h>
-		
-#include "main.h"
-		
 
-		
-/**
-		
- * _atoi - converts a string to an integer
-		
- * @s: string to be converted
-		
- *
-		
- * Return: the int converted from the string
-		
- */
-		
-int _atoi(char *s)
-		
+int main()
 {
-		
-	int i, d, n, len, f, digit;
-		
+    int n, i, sum = 0;
 
-		
-	i = 0;
-		
-	d = 0;
-		
-	n = 0;
-		
-	len = 0;
-		
-	f = 0;
-		
-	digit = 0;
-		
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
 
-		
-	while (s[len] != '\0')
-		
-		len++;
-		
+    // Error: missing condition to check if n is a positive integer
+    if(n < 0) {
+        printf("Error: Entered number is not a positive integer\n");
+        return 0;
+    }
 
-		
-	while (i < len && f == 0)
-		
-	{
-		
-		if (s[i] == '-')
-		
-			++d;
-		
+    for (i = 1; i <= n; ++i) {
+        sum += i;
+    }
 
-		
-		if (s[i] >= '0' && s[i] <= '9')
-		
-		{
-		
-			digit = s[i] - '0';
-		
-			if (d % 2)
-		
-				digit = -digit;
-		
-			n = n * 10 + digit;
-		
-			f = 1;
-		
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-		
-				break;
-		
-			f = 0;
-		
-		}
-		
-		i++;
-		
-	}
-		
+    printf("Sum = %d\n", sum);
 
-		
-	if (f == 0)
-		
-		return (0);
-		
-
-		
-	return (n);
-		
-}
-		
-
-		
-/**
-		
- * main - multiplies two numbers
-		
- * @argc: number of arguments
-		
- * @argv: array of arguments
-		
- *
-		
- * Return: 0 (Success), 1 (Error)
-		
- */
-		
-int main(int argc, char *argv[])
-		
-{
-		
-	int result, num1, num2;
-		
-
-		
-	if (argc < 3 || argc > 3)
-		
-	{
-		
-		printf("Error\n");
-		
-		return (1);
-		
-	}
-		
-
-		
-	num1 = _atoi(argv[1]);
-		
-	num2 = _atoi(argv[2]);
-		
-	result = num1 * num2;
-		
-
-		
-	printf("%d\n", result);
-		
-
-		
-	return (0);
-		
+    return 0;
 }
 
