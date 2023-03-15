532802af9c1aee0b570f2002dec23aa2453f849a
Cloggings-James <James.wanjirukenyan254@gmail.com>
2023-03-15 07:33:46 -0700

    hsd

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,49 +1,48 @@
-/**
- * strtow - Splits a string into words.
- * @str: The string to split.
- *
- * Return: Pointer to an array of strings (words), or NULL if str is NULL or empty.
- */
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
 char **strtow(char *str)
 {
-        char **words, *word;
-        int i, j, k, n, len;
+    char **words, *word;
+    size_t i, j, k, n, len;
 
-        if (str == NULL || *str == '\0')
-                return (NULL);
+    if (str == NULL || *str == '\0')
+        return (NULL);
 
-        len = strlen(str);
-        words = malloc(sizeof(char *) * (len + 1));
-        if (words == NULL)
-                return (NULL);
+    len = strlen(str);
+    words = malloc(sizeof(char *) * (len + 1));
+    if (words == NULL)
+        return (NULL);
 
-        for (i = 0, n = 0; i < len; i = j + 1)
+    for (i = 0, n = 0; i < len; i = j + 1)
+    {
+        while (i < len && isspace(str[i]))
+            i++;
+        if (i == len)
+            break;
+
+        j = i;
+        while (j < len && !isspace(str[j]))
+            j++;
+        word = (char *)malloc(sizeof(char) * (j - i + 1));
+        if (word == NULL)
         {
-                while (i < len && isspace(str[i]))
-                        i++;
-                if (i == len)
-                        break;
-
-                j = i;
-                while (j < len && !isspace(str[j]))
-                        j++;
-                word = malloc(sizeof(char) * (j - i + 1));
-                if (word == NULL)
-                {
-                        for (k = 0; k < n; k++)
-                                free(words[k]);
-                        free(words);
-                        return (NULL);
-                }
-
-                for (k = 0; i < j; k++, i++)
-                        word[k] = str[i];
-                word[k] = '\0';
-
-                words[n++] = word;
+            for (k = 0; k < i; k++)
+                free(words[k]);
+            free(words);
+            return (NULL);
         }
-        words[n] = NULL;
 
-        return (words);
+        for (k = 0; i < j; k++, i++)
+            word[k] = str[i];
+        word[k] = '\0';
+
+        words[n++] = word;
+    }
+    words[n] = NULL;
+
+    return (words);
 }
 
