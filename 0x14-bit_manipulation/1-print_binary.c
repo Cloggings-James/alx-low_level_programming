0554599d1c8c2043d20fe807d8a472b73620c922
james <james.wanjiru.sheba@gmail.com>
2023-04-09 10:56:28 -0700

    pdfate

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: the number to convert to binary and print
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask = 1;
+	int i;
+
+	/* Find the most significant bit */
+	for (i = 0; i < (sizeof(unsigned long int) * 8) - 1; i++)
+		mask = (mask << 1);
+
+	/* Print each bit starting with the most significant */
+	for (; i >= 0; i--)
+	{
+		if ((n & mask) == 0)
+			_putchar('0');
+		else
+			_putchar('1');
+		mask = (mask >> 1);
+	}
+}
+
