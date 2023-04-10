9224f1c90b041e0454d3329b18581e045c4f2c32
james <james.wanjiru.sheba@gmail.com>
2023-04-10 03:11:23 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,26 +7,28 @@
 
 int create_file(const char *filename, char *text_content)
 {
-    int fd, len, res;
-    mode_t mode = S_IRUSR | S_IWUSR;
+int fd, len, res;
+mode_t mode = S_IRUSR | S_IWUSR;
 
-    if (filename == NULL)
-        return (-1);
+if (filename == NULL)
+return (-1);
 
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode);
-    if (fd == -1)
-        return (-1);
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode);
+if (fd == -1)
+return (-1);
 
-    if (text_content != NULL) {
-        len = strlen(text_content);
-        res = write(fd, text_content, len);
-        if (res == -1) {
-            close(fd);
-            return (-1);
-        }
-    }
+if (text_content != NULL)
+{
+len = strlen(text_content);
+res = write(fd, text_content, len);
+if (res == -1)
+{
+close(fd);
+return (-1);
+}
+}
 
-    close(fd);
-    return (1);
+close(fd);
+return (1);
 }
 
