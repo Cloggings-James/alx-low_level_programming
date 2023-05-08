4ae63bb6246c30885000dd56d93486abc14cb226
Cloggings-James <james.wanjiru.sheba@gmail.com>
2023-05-08 08:44:52 -0700

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,53 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: the name of the file to read
- * @letters: the number of letters to read and print
+ * read_textfile - Reads a text file and prints
+ * @filename: A pointer to a string containing the name of the file to read.
+ * @letters: The number of letters to read and print.
  *
- * Return: the actual number of letters it could read and print, or 0 if an error occurred
+ * Return: On success, returns the actual number of letters read and printed.
+ *         On failure, returns 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *fp;
-	char *buffer;
+	int fd;
 	ssize_t bytes_read, bytes_written;
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
-	fp = fopen(filename, "r");
-	if (fp == NULL)
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
 		return (0);
 
-	buffer = malloc(letters + 1);
+	buffer = malloc(sizeof(char) * (letters + 1));
+
 	if (buffer == NULL)
 	{
-		fclose(fp);
+		close(fd);
 		return (0);
 	}
 
-	bytes_read = fread(buffer, 1, letters, fp);
+	bytes_read = read(fd, buffer, letters);
+	close(fd);
+
 	if (bytes_read == -1)
 	{
 		free(buffer);
-		fclose(fp);
 		return (0);
 	}
 
 	buffer[bytes_read] = '\0';
 
-	bytes_written = printf("%s", buffer);
-	if (bytes_written == -1 || bytes_written != bytes_read)
-	{
-		free(buffer);
-		fclose(fp);
-		return (0);
-	}
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 
 	free(buffer);
-	fclose(fp);
+
+	if (bytes_written == -1 || bytes_read != bytes_written)
+		return (0);
+
 	return (bytes_written);
 }
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,35 +1,37 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include "main.h"
+#include <string.h>
 
-/**
- * create_file - creates a file with the given text content
- * @filename: the name of the file to create
- * @text_content: the text content to write to the file
- *
- * Return: 1 on success, or -1 on failure
- */
-int create_file(const char *filename, char *text_content)
-{
-	if (filename == NULL)
-		return (-1);
+int create_file(const char *filename, char *text_content) {
+    int fd, write_status;
+    int permissions = S_IRUSR | S_IWUSR; /* rw------- */
 
-	int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd == -1)
-		return (-1);
+    if (filename == NULL)
+        return -1;
 
-	if (text_content != NULL)
-	{
-		size_t len = 0;
-		while (text_content[len] != '\0')
-			len++;
+    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, permissions);
+    if (fd == -1)
+        return -1;
 
-		ssize_t bytes_written = write(fd, text_content, len);
-		if (bytes_written == -1)
-		{
-			close(fd);
-			return (-1);
-		}
-	}
+    if (text_content != NULL) {
+        write_status = write(fd, text_content, strlen(text_content));
+        if (write_status == -1) {
+            close(fd);
+            return -1;
+        }
+    }
 
-	close(fd);
-	return (1);
+    close(fd);
+    return 1;
 }
+
+int main(void) {
+    /* Test the create_file function */
+    int result = create_file("example.txt", "Hello, World!");
+    printf("create_file result: %d\n", result);
+
+    return 0;
+}
+
