f70b526541cb21073f4ebcd7706dbdcc00e66941
Cloggings-James <James.wanjiru.sheba@gmail.com>
2023-06-19 08:07:17 -0700

    easther

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -1,14 +1,18 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - Entry point
+ * main - check the code
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
 {
-    char *str = "Hello, Dynamic Library!";
-    _puts(str);
-    return 0;
+	char *str = "Hello, Dynamic Libraries!";
+
+	_puts(str);
+	printf("Length: %d\n", _strlen(str));
+
+	return (0);
 }
 
--- 0x18-dynamic_libraries/main.c
+++ 0x18-dynamic_libraries/main.c
@@ -1,45 +1,23 @@
 #include <stdio.h>
-#include "main.h"
+#include "operations.h"
 
-int main() {
+int main(void)
+{
     int a = 10;
     int b = 5;
 
     int result_add = add(a, b);
-    printf("Addition: %d + %d = %d\n", a, b, result_add);
-
     int result_subtract = subtract(a, b);
-    printf("Subtraction: %d - %d = %d\n", a, b, result_subtract);
-
     int result_multiply = multiply(a, b);
-    printf("Multiplication: %d * %d = %d\n", a, b, result_multiply);
-
     int result_divide = divide(a, b);
-    printf("Division: %d / %d = %d\n", a, b, result_divide);
-
     int result_modulus = modulus(a, b);
-    printf("Modulus: %d %% %d = %d\n", a, b, result_modulus);
+
+    printf("Addition: %d\n", result_add);
+    printf("Subtraction: %d\n", result_subtract);
+    printf("Multiplication: %d\n", result_multiply);
+    printf("Division: %d\n", result_divide);
+    printf("Modulus: %d\n", result_modulus);
 
     return 0;
-}
-
-int add(int a, int b) {
-    return a + b;
-}
-
-int subtract(int a, int b) {
-    return a - b;
-}
-
-int multiply(int a, int b) {
-    return a * b;
-}
-
-int divide(int a, int b) {
-    return a / b;
-}
-
-int modulus(int a, int b) {
-    return a % b;
 }
 
