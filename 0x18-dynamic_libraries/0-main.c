ba124c943b90a1d2615274f87e4d5cabd431f61c
Cloggings-James <James.wanjiru.sheba@gmail.com>
2023-06-20 05:15:13 -0700

    muchemi

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h> // Include the <stdlib.h> header
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    printf("%d\n", _strlen("My Dyn Lib"));
+    return EXIT_SUCCESS; // Use EXIT_SUCCESS directly
+}
+
